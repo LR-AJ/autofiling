99dfb8471f41d37b3d89eb0f3436f2098996263f
LR-AJ <legalraastatech3@gmail.com>
2023-07-06 16:31:23 +0530

    Update in service

--- mail/customerMail.php
+++ mail/customerMail.php
@@ -88,35 +88,6 @@ if (isset($_POST['name']) && isset($_POST['email'])) {
 				</div>
 			</body>
 		</html>';
-	  	}else if(isset($_POST['service_code']) && $service_code == '001') {
-			$Body='<html>
-			<body>
-				<div>Dear '.$_POST['name'].'
-					<p>We wanted to take a moment to thank you for choosing our '. $service_name[$service_code].'. We appreciate the trust you have placed in Autofiling, and we are committed to delivering the highest quality service in setting up your business in Dubai. Additionally, we also provide business setup services in Free Zone, Mainland and Offshore areas. <br>
-					We are grateful for the opportunity to work with you and for the positive impact that our License services have had on your business. Your satisfaction is Autofiling’s top priority, and we are dedicated to ensuring that you receive the best possible experience with our company. Click to know about our services and us</p>
-				
-					<p><strong>About Autofiling:<br></strong>With the goal of providing support for setting up your offshore business anywhere in the world, Autofillings was founded in 2017</p>
-					<p class="mb-4">Our services include banking assistance, accounting & auditing, legal administration, LLC formation, tax registration certificate, and assistance for obtaining visas and work permits, among other things. We also specialize in incorporating offshore companies, opening offshore branches, creating trusts in offshore jurisdictions, opening an offshore entity, and opening an offshore entity.</p>
-					<p>With almost every imaginable company scenario, our team s expertise has managed virtually every type of business and has assisted clients in operating processes smoothly and effectively.</p>
-
-					<p>Warm regards,<br>Team, Autofiling<br><em></em>
-					<p>Contact Us</p>
-						Call Us: <a href="tel:+971585606800" data-mce-href="tel:+971585606800">+971585606800</a>&nbsp;|&nbsp; <a href="tel:+918882038525">+918882038525</a>  <br> <br>
-						Site:<a href="https://test.legalraasta.com/autofile" data-mce-href="https://test.legalraasta.com/autofile">www.Autofile.com</a><br> <br>
-						Head Office :<a href="https://goo.gl/maps/T8roTQnp2RtndkPj6"> B-300 Saraswati Vihar,Pitampura, New Delhi-110034, Delhi </a></p>
-							
-							<a  href="https://twitter.com/RaastaLegal"
-                                aria-label="Twitter"><i class="fab fa-twitter fw-normal"> Twitter</i></a>
-                            <a  href="https://www.facebook.com/LRaasta"
-                                aria-label="facebook"><i class="fab fa-facebook-f fw-normal"> Facebook</i></a>
-                            <a 
-                                href="https://www.linkedin.com/company/legalraasta/mycompany/"
-                                aria-label="linkedn"><i class="fab fa-linkedin-in fw-normal">Linkedin</i></a>
-                            <a href="https://www.instagram.com/legal.raasta/"
-                                aria-label="pinterest"><i class="fab fa-pinterest fw-normal"></i>Instagram</a>
-				</div>
-			</body>
-		</html>';
 		}else if(isset($_POST['service_code']) && $service_code == '002') {
 			$Body='<html>
 			<body>
